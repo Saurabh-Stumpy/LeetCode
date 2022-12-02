72757210219145975c9515a0a1760db03544587d
Saurabh Walanjuwani <walanjuwani@gmail.com>
2022-12-02 17:18:08 +0530

    Time: 13 ms (92.44%), Space: 43.6 MB (80.44%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean closeStrings(String word1, String word2) {
        if(word1.length()!=word2.length())return false;
 
-        int[] arr1 = new int[27];
-        int[] arr2 = new int[27];
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
 
         for(int i=0;i<word1.length();i++){
             arr1[word1.charAt(i)-'a']++;
