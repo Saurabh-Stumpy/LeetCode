b4fdc9ab628348c35f89e73d7e05fbf4a2e5917a
Saurabh Walanjuwani <walanjuwani@gmail.com>
2022-12-02 17:17:37 +0530

    Time: 34 ms (49.33%), Space: 54.7 MB (64.00%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -18,10 +18,6 @@ class Solution {
         Arrays.sort(arr1);
         Arrays.sort(arr2);
 
-        for(int i=0;i<arr1.length;i++){
-            if(arr1[i]!=arr2[i]) return false;
-        }
-
-        return true;
+        return Arrays.equals(arr1,arr2);
     }
 }
