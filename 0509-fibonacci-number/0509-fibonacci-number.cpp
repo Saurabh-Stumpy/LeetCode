4a59637e79af48cf9310af0304836e1546bdaf0f
Saurabh Walanjuwani <walanjuwani@gmail.com>
2022-12-12 09:07:21 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (15.43%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,6 +3,14 @@ public:
     int fib(int n) {
         if(n==0 || n==1) return n;
         
-        return fib(n-1)+fib(n-2);
+        int a=0,b=1,c=0;
+        for(int i=1;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+        
+        //return fib(n-1)+fib(n-2);
     }
 };
