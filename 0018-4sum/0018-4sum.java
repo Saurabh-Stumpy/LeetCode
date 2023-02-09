6ae9ebf1d66ee1a75e4dada2eb34582f750a0d88
Saurabh Walanjuwani <walanjuwani@gmail.com>
2023-02-09 13:35:58 +0530

    Time: 22 ms (59.16%), Space: 42.7 MB (79.91%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> arr = new ArrayList<>();
+
+        Arrays.sort(nums);
+//        for(int a:nums){
+//            System.out.print(a+" ");
+//        }
+//        System.out.println();
+        for (int i=0;i<nums.length-3;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for (int j=i+1;j<nums.length-2;j++){
+                if(j-i>1 && nums[j]==nums[j-1]) continue;
+                int start = j+1;
+                int end = nums.length-1;
+                while (start<end){
+//                    System.out.println(nums[i]+" "+nums[j]+" "+nums[start]+" "+nums[end]+" "+(nums[start]+nums[end]+nums[j]+nums[i]));
+                    if((long)nums[start]+nums[end]+nums[j]+nums[i]==target){
+                        List<Integer> li = new ArrayList<>();
+                        li.add(nums[end]);
+                        li.add(nums[start]);
+                        li.add(nums[j]);
+                        li.add(nums[i]);
+                        arr.add(li);
+                        do{
+                            start++;
+                        }while (nums[start]==nums[start-1]&& start<end);
+                        do{
+                            end--;
+                        }while (nums[end]==nums[end+1] && end>start);
+                    }
+                    else if(nums[start]+nums[end]+nums[j]+nums[i]<target) start++;
+                    else end--;
+                }
+            }
+
+        }
+
+        return arr;
+    }
+}
